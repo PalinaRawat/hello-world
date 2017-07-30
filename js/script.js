cc6f02dad219fcd5015fc94a94f1be526b28ada6
Palina Rawat <palina-rawat@mail.ru>
2017-07-29 19:41:56 -0700

    more changes

--- js/script.js
+++ js/script.js
@@ -20,3 +20,19 @@ $div
 
 window.onload = withVelocity;
 
+$(document).ready(function () {
+    // bind click event to all internal page anchors
+    $('a[href*="#"]').on('click', function (e) {
+        // prevent default action and bubbling
+        e.preventDefault();
+        e.stopPropagation();
+        // set target to anchor's "href" attribute
+        var target = $(this).attr('href');
+        // scroll to each target
+        $(target).velocity('scroll', {
+            duration: 500,
+            offset: -40,
+            easing: 'ease-in-out'
+        });
+    });
+});
